9b5720decb5b6381f6b7a95e9e920834deb76607
Pedro Miguel <rudaprod.oficial@gmail.com>
2024-03-03 19:06:51 -0300

    Possible Moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -36,14 +36,13 @@ public class UI {
 	 System.out.flush();
 	} 
 	
-	public static ChessPosition readChessPosition(Scanner sc) {
-		
+	public static ChessPosition readChessPosition(Scanner sc) {		
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
-	    }
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		    }
 		catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,12 +1,13 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
 	
 	public Piece(Board board) {
-		this.board = board;	
+		this.board = board;
+		position = null;
 	}
 
 	protected Board getBoard() {
@@ -14,4 +15,23 @@ public class Piece {
 	}
 	
 	
-}
+	public abstract boolean[][] possibleMoves();
+		 
+	public boolean possibleMove(Position position) {
+	 return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+	public boolean IsThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i < mat.length; i++ ) {
+			for (int j=0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+				
+			}
+		}
+		return false;
+	}
+ }
+
